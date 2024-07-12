8d82f004237b8fb82c6daa6f85f938086034685e
najim2004 <najim10112004@gmail.com>
2024-07-12 11:21:07 +0600

    add base url

--- src/hooks/useAxiosPublic.jsx
+++ src/hooks/useAxiosPublic.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 const axiosPublic = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
 });
 
 const useAxiosPublic = () => {
