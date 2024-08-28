04129d9fdc3000847ed860e92d778ce3853f1f1d
najim2004 <najim10112004@gmail.com>
2024-08-28 23:23:02 +0600

    fixed deployment error

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,9 @@
 "use client";
 import Container from "@/components/ui/Container";
 import HeroSection from "./home/herosection/HeroSection";
-import Categories from "./home/categories/Categories";
 import HowToOrder from "./home/howtoorder/HowToOrder";
-import ProductSlider from "@/components/productslider/productslider";
+import ProductSlider from "@/components/productslider/Productslider";
+import Categories from "./home/categories/Categories";
 
 const Home = () => {
   return (
